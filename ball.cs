49d0fee8f0705a163c811214283ba4f429415992
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2020-06-13 02:24:09 +0900

    Add files via upload

---
+++ ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ball : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var power = (transform.forward + transform.right) * speed;
+        GetComponent<Rigidbody>().AddForce(power, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 30 || transform.position.x < -30)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
